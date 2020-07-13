d20b0d61c90d13fa754357b43ef2841159e524b1
volodymyr <kokolus7806@gmail.com>
2020-07-13 18:29:58 +0300

    create task 3-4

---
+++ main/task4/main.js
@@ -0,0 +1,25 @@
+// --Завантажити з мережі будь-який шаблон сайту. Підключити до нього свій скріпт-файл. У файлі прописати наступні доступи та дії
+//  - знайти всі елементі, які мають class
+
+// let clas  = document.getElementsByTagName("*");
+// let arrayClass = [];
+// for(i = 0; i < clas.length; i++){
+//     if(clas[i].className !== "")
+//         {
+//             arrayClass.push(clas[i]);
+//         }
+// }
+// console.log(arrayClass);
+
+//  - знайти всі параграфи ,та змінити текст на hello oktenweb!
+// let parag = document.getElementsByTagName('p');
+
+// for (const element of parag) {
+//     element.innerHTML = 'hello okteneb';
+// }
+
+// //  - знайти всі div та змінити ім колір на червоний
+// let div = document.getElementsByTagName('div');
+// for (const element of div) {
+//     element.style.background = 'red';
+// }
