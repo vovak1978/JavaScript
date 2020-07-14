5cb86ebfe2fa38867a8b856d683a824392ce4bc8
volodymyr <kokolus7806@gmail.com>
2020-07-15 00:11:41 +0300

    finish task4

--- main/lessons4/main.js
+++ main/lessons4/main.js
@@ -124,21 +124,62 @@
 
 // - Створити функцію яка приймає масив будь яких объектів, та 
 // повертає значення кількості об'єктів в масиві
-
+// function lengthArray(array) {
+//     quantity = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         if(typeof array[i] === "object" ){
+//             quantity = quantity + 1;
+//                     }
+        
+//     }
+//     console.log(quantity);
+//         return(quantity);
+// }
 
 // - Створити функцію яка приймає масив будь яких объектів, та повертає 
 // загальн кількість полів в них
-
-// - створити функцію  яка скаладає значення елементів з однаковими індексами  
+// function fieldArray(array) {
+    //     field = 0;
+    //     for (const element of array){
+    //         for (const key in element) {
+    //             field = field + 1;
+    //         }
+    //       }
+        
+    //     console.log(field);
+    //         return(field);
+    // }
+    
+  // - створити функцію  яка скаладає значення елементів з однаковими індексами  
 // та повертає новий результуючий масив.
 //   Приклад
 //   [1,2,3,4]
 //   [2,3,4,5]
 //   результат
 //   [3,5,7,9]
+// function addElementsOfArray(array, array1) {
+//     let array2 = [];
+//     for (let i = 0; i < array.length; i++) {
+//         array2.push(array[i] + array1[i]);
+//      }
+//      console.log(array2);
+//      return(array2);
+// }
 
-// - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в 
-// індексі "i" на "i+1"
+
+// // - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в 
+// // індексі "i" на "i+1"
+// function changePlace(array, i) {
+//     let firstNumber = array[i];
+//     let secondNumber = array[i+1];
+//     array[i] = secondNumber;
+//     array[i + 1] = firstNumber;
+//     console.log(array);
+//     return(array)
+// }
+// let array = [0,1,2,3,4,5,6]
+// changePlace(array, 4)
+
 // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. 
 // Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
@@ -146,20 +187,107 @@
 // [1,0,6,0,3] => [1,6,3,0,0]
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
+// function zeroToEnd(array) {
+//     for (let i = array.length; i > -1; i--) {
+        
+//         if(array[i] === 0){
+//         array.splice(i, 1);
+//         array.push(0);    
+//         }
+//     }
+//     console.log(array)
+//     return(array)
+// }
+
+// let array = [1,0,6,0,3];
+// zeroToEnd(array);
 
 // Створити функцію яка :
 // - Додає в боді блок з текстом "Hello owu"
+// function addTextToDiv(text) {
+//     let div = document.createElement("div");
+//     div.innerText  = (text);
+//     document.body.appendChild(div)
+// }
+
+// addTextToDiv("Hello owu");
+
 // - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
-// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-// Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
-// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-// Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
+// function addElementToBody(element, text) {
+//         element = document.createElement(element);
+//         element.innerText = (text);
+//         document.body.appendChild(element)
+//     }
+    
+//     addElementToBody("p","Hello");
+
+// - приймає масив автомобілів (можна взяти з попередніх дз ),
+// та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
+// function addArrayToBody(array, element) {
+//     element = document.createElement(element);
+//     let list = [];
+//     for (const iter of array) {
+//        
+//        list.push(iter.model);
+//     }
+
+//     element.innerText = (list);
+//     document.body.appendChild(element);
+// }
+ 
+// let cars =[
+//         {model: 'AUDI', year: 1995, power: 1500, color: 'black'},
+//         {model: 'VW', year: 1999, power: 1500, color: 'black'},
+//         {model: 'SIAT', year: 1990, power: 1500, color: 'white'},
+//         {model: 'KIA', year: 1991, power: 1500, color: 'black'},
+//         {model: 'Nissan', year: 1993, power: 2000, color: 'black'},
+//         {model: 'Honda', year: 1998, power: 1500, color: 'black'},
+//         {model: 'Mazda', year: 1997, power: 1500, color: 'black'},
+//         {model: 'Huyndai', year: 1996, power: 1600, color: 'black'},
+//         {model: 'Scoda', year: 1998, power: 1500, color: 'black'},
+//         {model: 'Tesla', year: 2015, power: 1500, color: 'black'}
+//     ]
+
+    // addArrayToBody(cars, "div")
+
+// Для кожного автомобіля створити свій блок, та додати його в елемент, 
+// індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+// function addArrayBody(array, element) {
+//     element = document.createElement(element);  
+//     for (const iter of array) {
+//         let p = document.createElement("p")
+//         p.innerText = JSON.stringify(iter);
+//         element.appendChild(p);
+       
+//     }
+//     document.body.appendChild(element);
+    
+    
+// }
+
+// addArrayBody(cars, "div")
+
+// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор
+//  елемнту в який потрібно додати список цих автомобілів.
+// Для кожного автомобіля створити свій блок, та додати його в елемент, 
+// індентифікатор якого ви отримали.
 // Для кожної властивості створити всередені блока автомоблія свій блок
 
-// (на основі минулого ДЗ)
-// **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
-// та повертає масив цих з'єднаних об'єктів.
-// Приклад масивів:
-//             let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-//             let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
-// Частковий приклад реультату:
+// function addArrayBlock(array, element) {
+//     element = document.createElement(element);
+//     for (const elem of array) {
+//        let div = document.createElement("div");
+//        element.appendChild(div);
+//        for (const key in elem) {
+           
+//            let p = document.createElement("p");
+//            p.innerText = (JSON.stringify(elem[key]))
+//            div.appendChild(p);
+               
+//            }
+//        }
+    
+//     document.body.appendChild(element)
+// }
+
+// addArrayBlock(cars, "div")
