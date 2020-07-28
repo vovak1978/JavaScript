34809ad4bfadc76d6c1c89a05136a9fb60ab7b1e
volodymyr <kokolus7806@gmail.com>
2020-07-28 15:06:08 +0300

    add two files to task8

---
+++ main/lesson8/main.js
@@ -0,0 +1,71 @@
+// Дана textarea.
+// В неё вводится текст.
+// Сделайте так, чтобы после захода на эту страницу через некоторое время,
+//  введенный текст остался в textarea.
+// const area = document.querySelector('#store');
+// area.value = localStorage.getItem('item');
+
+// window.onbeforeunload = () => {
+//     localStorage.setItem('item', area.value);
+// }
+
+// - Дана форма с инпутами, текстареа, чекбоксами, радио кнопочками, селектами и тп.
+// Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
+// Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли 
+// на своих местах.
+// Сделайте ваш скрипт как можно более универсальным.
+
+// -Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" 
+// и они "фикисруются" (в хранилище), затем поредактировать их, 
+// затем еще поредактировать и возможно еще.....
+// Требование : хранить историю своих изменений (даже после перезагрузки страницы).
+// Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться 
+// по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
+
+// const area = document.querySelector('#store');
+// const btn = document.querySelector('#btn');
+// const div =  document.querySelector('#content')
+
+// const btnLeft = document.createElement("button");
+// btnLeft.innerText = ('Назад');
+// div.appendChild(btnLeft);
+// const btnRight = document.createElement("button");
+// btnRight.innerText = ('Вперед');
+// div.appendChild(btnRight);   
+// btnRight.hidden = true;
+// btnLeft.hidden = true;
+
+// if (localStorage.length > 0){
+//   i = localStorage.length;
+//   j = i;
+//   area.value = localStorage.getItem(i);  
+//   btnRight.hidden = false;
+//   btnLeft.hidden = false;
+// }
+//    else{
+//     i = 0;
+//    }
+     
+// btn.onclick = () => {
+//     i = i + 1;   
+//     j = i; 
+//     localStorage.setItem(i, area.value);
+//    btnRight.hidden = false;
+//    btnLeft.hidden = false;
+//     }
+
+
+// btnLeft.onclick = () => {
+//    if (j > 1) { 
+//     j = j - 1;
+//   area.value = localStorage.getItem(j);
+// }
+// }
+
+// btnRight.onclick = () => {
+//     if (j < i){
+//     j = j + 1;
+//   area.value = localStorage.getItem(j);
+//     }
+// }
+
