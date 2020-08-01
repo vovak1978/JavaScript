bd647fe7961f642cc53d46168e973e7753e7aef6
volodymyr <kokolus7806@gmail.com>
2020-08-01 22:38:31 +0300

    recoment code

--- main/lesson8/main.js
+++ main/lesson8/main.js
@@ -17,50 +17,51 @@
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться 
 // по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
 
-// const area = document.querySelector('#store');
-// const btn = document.querySelector('#btn');
-// const div =  document.querySelector('#content')
+localStorage.clear();
+const area = document.querySelector('#store');
+const btn = document.querySelector('#btn');
+const div =  document.querySelector('#content')
 
-// const btnLeft = document.createElement("button");
-// btnLeft.innerText = ('Назад');
-// div.appendChild(btnLeft);
-// const btnRight = document.createElement("button");
-// btnRight.innerText = ('Вперед');
-// div.appendChild(btnRight);   
-// btnRight.hidden = true;
-// btnLeft.hidden = true;
+const btnLeft = document.createElement("button");
+btnLeft.innerText = ('Назад');
+div.appendChild(btnLeft);
+const btnRight = document.createElement("button");
+btnRight.innerText = ('Вперед');
+div.appendChild(btnRight);   
+btnRight.hidden = true;
+btnLeft.hidden = true;
 
-// if (localStorage.length > 0){
-//   i = localStorage.length;
-//   j = i;
-//   area.value = localStorage.getItem(i);  
-//   btnRight.hidden = false;
-//   btnLeft.hidden = false;
-// }
-//    else{
-//     i = 0;
-//    }
+if (localStorage.length > 0){
+  i = localStorage.length;
+  j = i;
+  area.value = localStorage.getItem(i);  
+  btnRight.hidden = false;
+  btnLeft.hidden = false;
+}
+   else{
+    i = 0;
+   }
      
-// btn.onclick = () => {
-//     i = i + 1;   
-//     j = i; 
-//     localStorage.setItem(i, area.value);
-//    btnRight.hidden = false;
-//    btnLeft.hidden = false;
-//     }
+btn.onclick = () => {
+    i = i + 1;   
+    j = i; 
+    localStorage.setItem(i, area.value);
+   btnRight.hidden = false;
+   btnLeft.hidden = false;
+    }
 
 
-// btnLeft.onclick = () => {
-//    if (j > 1) { 
-//     j = j - 1;
-//   area.value = localStorage.getItem(j);
-// }
-// }
+btnLeft.onclick = () => {
+   if (j > 1) { 
+    j = j - 1;
+  area.value = localStorage.getItem(j);
+}
+}
 
-// btnRight.onclick = () => {
-//     if (j < i){
-//     j = j + 1;
-//   area.value = localStorage.getItem(j);
-//     }
-// }
+btnRight.onclick = () => {
+    if (j < i){
+    j = j + 1;
+  area.value = localStorage.getItem(j);
+    }
+}
 
